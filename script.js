45f4f5f0dc8d1ce8d78ed9950dcf8c08d8359ffd
Nahid Khan <nahidkhanniloy31@gmail.com>
2026-02-16 04:13:02 +0600

    added some pages like banner, service and faq section, and basic js setup

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    fetchTrending();
+    fetchCategories();
+};
+
+function showSection(sectionId) {
+    const pages = ['home-page', 'product-page', 'contact-page', 'faq-page'];
+    // console.log("yes working", {pages})
+    
+}
